537314d75a0a75107b7253bcbda26ba03c4134bf
Seongho <chzhqk98@naver.com>
2025-12-31 14:26:31 +0900

    Refactor and simplify annotation handling
    - Remove `Role`, `Admin` annotations, and `Step0Reflection` class.
    - Eliminate `MergedAnnotation` interface.
    - Introduce `NotificationSender` and integrate it into `UserService`.
    - Add `Main` class to demonstrate user registration and notification handling.

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+        UserService userService = new UserService();
+        userService.register("seongho");
+    }
+}
--- src/UserService.java
+++ src/UserService.java
@@ -1,3 +1,13 @@
-@Admin
 public class UserService {
+
+    private final NotificationSender notificationSender;
+
+    public UserService() {
+        this.notificationSender = new NotificationSender();
+    }
+
+    public void register(String username) {
+        System.out.println("register user: " + username);
+        notificationSender.send("welcome " + username);
+    }
 }
